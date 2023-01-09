088e6510625c45e9d4457f1a5002a1bd3905d96e
mjfwebb <michael@w3bb.net>
2023-01-09 21:22:24 +0100

    feat: add follow reactions
    closes: #1

--- server/src/twitchWebsocket.ts
+++ server/src/twitchWebsocket.ts
@@ -3,10 +3,61 @@ import { getConnection } from './bot';
 import { REWARDS, TWITCH_WEBSOCKET_EVENTSUB_URL } from './constants';
 import { botCommands } from './handlers/botCommands';
 import { subscribeToRedeems } from './handlers/customRewards';
+import { subscribeToFollows } from './handlers/followHandler';
 import { sendChatMessage } from './helpers/sendChatMessage';
 import { playSound } from './playSound';
-import type { ChannelPointRedeemNotificatonEvent, TwitchWebsocketMessage } from './types';
+import type { ChannelPointRedeemNotificatonEvent, FollowNotificationEvent, TwitchWebsocketMessage } from './types';
 import { hasOwnProperty } from './utils/hasOwnProperty';
+
+function handleNotification(data: TwitchWebsocketMessage) {
+  if (hasOwnProperty(data.payload, 'event') && hasOwnProperty(data.payload.event, 'followed_at')) {
+    const event = data.payload.event as unknown as FollowNotificationEvent;
+    const connection = getConnection();
+    if (connection) {
+      sendChatMessage(connection, `Thank you for following ${event.user_name}, I love you`);
+    }
+  }
+
+  // ChannelPointRedeemNotificatonEvent
+  if (hasOwnProperty(data.payload, 'event') && hasOwnProperty(data.payload.event, 'reward')) {
+    const event = data.payload.event as unknown as ChannelPointRedeemNotificatonEvent;
+    const reward = Object.values(REWARDS).find((value) => value === event.reward.id);
+    switch (reward) {
+      case REWARDS.pushup:
+        playSound('redeem');
+        {
+          const connection = getConnection();
+          if (connection) {
+            sendChatMessage(connection, "It's time to get down");
+          }
+        }
+        break;
+      case REWARDS.pushupAddOne:
+        {
+          const addPushupCommand = botCommands.find((command) => command.command === 'addpushup');
+          const connection = getConnection();
+          if (addPushupCommand && connection) {
+            playSound('redeem');
+            addPushupCommand.callback(connection, {
+              tags: null,
+              source: null,
+              command: null,
+              parameters: null,
+            });
+          }
+        }
+        break;
+      case REWARDS.test:
+        {
+          playSound('redeem');
+        }
+        break;
+      default:
+        console.log('Unsupported reward');
+        break;
+    }
+  }
+}
 
 export function runTwitchWebsocket() {
   const client = new websocket.client();
@@ -36,50 +87,14 @@ export function runTwitchWebsocket() {
               const sessionId = data.payload.session?.id;
               if (sessionId) {
                 subscribeToRedeems(sessionId).catch((e) => console.error(e));
+                subscribeToFollows(sessionId).catch((e) => console.error(e));
               }
             }
 
             break;
 
           case 'notification':
-            if (hasOwnProperty(data.payload, 'event') && hasOwnProperty(data.payload.event, 'reward')) {
-              const event = data.payload.event as unknown as ChannelPointRedeemNotificatonEvent;
-              const reward = Object.values(REWARDS).find((value) => value === event.reward.id);
-              switch (reward) {
-                case REWARDS.pushup:
-                  playSound('redeem');
-                  {
-                    const connection = getConnection();
-                    if (connection) {
-                      sendChatMessage(connection, "It's time to get down");
-                    }
-                  }
-                  break;
-                case REWARDS.pushupAddOne:
-                  {
-                    const addPushupCommand = botCommands.find((command) => command.command === 'addpushup');
-                    const connection = getConnection();
-                    if (addPushupCommand && connection) {
-                      playSound('redeem');
-                      addPushupCommand.callback(connection, {
-                        tags: null,
-                        source: null,
-                        command: null,
-                        parameters: null,
-                      });
-                    }
-                  }
-                  break;
-                case REWARDS.test:
-                  {
-                    playSound('redeem');
-                  }
-                  break;
-                default:
-                  console.log('Unsupported reward');
-                  break;
-              }
-            }
+            handleNotification(data);
 
             break;
 
--- server/src/types.ts
+++ server/src/types.ts
@@ -22,6 +22,16 @@ export type ChannelPointRedeemNotificatonEvent = {
   };
 };
 
+export type FollowNotificationEvent = {
+  user_id: string; //	The user ID for the user now following the specified channel.
+  user_login: string; //	The user login for the user now following the specified channel.
+  user_name: string; //	The user display name for the user now following the specified channel.
+  broadcaster_user_id: string; //	The requested broadcaster ID.
+  broadcaster_user_login: string; //	The requested broadcaster login.
+  broadcaster_user_name: string; //	The requested broadcaster display name.
+  followed_at: string; //	RFC3339 timestamp of when the follow occurred.
+};
+
 export type TwitchWebsocketSessionData = {
   id: string;
   status: 'connected';
@@ -144,7 +154,7 @@ export type UserStateTags = {
 };
 
 export type NoticeTags = {
-  'msg-id': string;
+  'msg-id': string | 'resub';
   'target-user-id': string;
 };
 
