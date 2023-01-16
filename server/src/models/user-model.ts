4527731dd9e95710f0da2196e815a41d9c52660c
mjfwebb <michael@w3bb.net>
2023-01-16 20:44:51 +0100

    refactor: make welcome message potentially undefined

--- server/src/models/user-model.ts
+++ server/src/models/user-model.ts
@@ -4,7 +4,7 @@ import { model, Schema } from 'mongoose';
 const UserSchema = new Schema<User>(
   {
     userId: { type: String, required: true },
-    welcomeMessage: { type: String, required: true, default: 'Welcome back' },
+    welcomeMessage: { type: String, required: false },
     points: { type: Number, required: true, default: 0 },
     experience: { type: Number, required: true, default: 0 },
     lastSeen: { type: String, required: true, default: '0' },
@@ -16,7 +16,7 @@ const UserSchema = new Schema<User>(
 
 export interface User extends SchemaTimestampsConfig {
   userId: string;
-  welcomeMessage: string;
+  welcomeMessage?: string;
   points: number;
   experience: number;
   lastSeen: string;
