3ea32ef45d48a28c1f91bc50f18bedc3c242ccd2
mjfwebb <michael@w3bb.net>
2023-01-18 13:38:22 +0100

    feat: add !play command

--- server/src/botCommands.ts
+++ server/src/botCommands.ts
@@ -533,6 +533,16 @@ export const botCommands: BotCommand[] = [
     },
   },
   {
+    command: 'play',
+    id: 'play',
+    description: 'Get the play link for Between Worlds',
+    callback: (connection) => {
+      const links = ['https://www.betweenworlds.net', 'https://athanoquest.com', 'https://athano.quest', 'https://nice.gg'];
+      const number = getRandomNumberInRange(0, 3);
+      sendChatMessage(connection, links[number]);
+    },
+  },
+  {
     command: 'command',
     id: 'command',
     description: 'Use this command to find out more about a command',
