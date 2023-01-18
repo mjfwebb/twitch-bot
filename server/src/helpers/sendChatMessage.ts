a1fd1eb9ba184a2ce93b4c8a9978d814318d04c1
mjfwebb <michael@w3bb.net>
2023-01-18 13:16:26 +0100

    feat: add sending multiple messages for long messages (over 450 char)
    closes: #36

--- server/src/helpers/sendChatMessage.ts
+++ server/src/helpers/sendChatMessage.ts
@@ -4,11 +4,31 @@ import Config from '../config';
 const channel = `#${Config.twitch.channel}`; // Replace with your channel.
 
 export function sendChatMessage(connection: websocket.connection, message: string, amount = 1) {
-  if (amount === 1) {
-    connection.send(`PRIVMSG ${channel} :${message}`);
-    return;
+  if (message.length < 450) {
+    sendMessage(message);
+  } else {
+    const messageWords = message.split(' ');
+    let newMessage = '';
+    messageWords.forEach((word) => {
+      if (newMessage.length + word.length > 450) {
+        sendMessage(newMessage);
+        newMessage = word;
+      } else {
+        newMessage = `${newMessage} ${word}`;
+      }
+    });
+    if (newMessage.length > 0) {
+      sendMessage(newMessage);
+    }
   }
-  for (let x = 0; x < amount; x += 1) {
-    connection.send(`PRIVMSG ${channel} :${message}`);
+
+  function sendMessage(message: string) {
+    if (amount === 1) {
+      connection.send(`PRIVMSG ${channel} :${message}`);
+      return;
+    }
+    for (let x = 0; x < amount; x += 1) {
+      connection.send(`PRIVMSG ${channel} :${message}`);
+    }
   }
 }
