8c9d667ef570ec82a0b0835e607e5b43c8d86bae
mjfwebb <michael@w3bb.net>
2023-02-15 23:45:13 +0100

    feat: update test cases for more punctuation marks

--- server/src/commands/helpers/generateCommandMessage.test.ts
+++ server/src/commands/helpers/generateCommandMessage.test.ts
@@ -2,21 +2,27 @@ import type { Command } from '../../models/command-model';
 import { generateCommandMessage } from './generateCommandMessage';
 
 describe('generateCommandMessage', () => {
-  test('should generate a message for the "test" command', () => {
-    const commandData: Command = {
-      commandId: 'testComand',
-      timesUsed: 10,
-    };
-    const message = generateCommandMessage(
-      'test',
-      {
-        command: 'test',
-        id: 'test',
-        description: 'This test description ends in an exclamation mark!',
-        callback: () => false,
-      },
-      commandData,
-    );
-    expect(message).not.toContain('!.');
-  });
+  const punctuationMarks = ['..', ':', '?', '!'];
+
+  const commandData: Command = {
+    commandId: 'testComand',
+    timesUsed: 10,
+  };
+
+  for (const punctuationMark of punctuationMarks) {
+    test(`should make sure is a single punctuation mark when using ${punctuationMark}`, () => {
+      const message = generateCommandMessage(
+        'test',
+        {
+          command: 'test',
+          id: 'test',
+          description: `This test description ends in an punctuation mark${punctuationMark}`,
+          callback: () => false,
+        },
+        commandData,
+      );
+      expect(message).not.toContain(`punctuation mark${punctuationMark}.`);
+      expect(message).toContain(`punctuation mark${punctuationMark} `);
+    });
+  }
 });
