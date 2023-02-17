55e2bc8b4cc371d809af54e39da2ec3f9daf0701
mjfwebb <michael@w3bb.net>
2023-02-17 16:46:32 +0100

    fix: use bot command params in help command

--- server/src/commands/command.ts
+++ server/src/commands/command.ts
@@ -1,6 +1,8 @@
 import type { BotCommand } from '../types';
+import { findBotCommand } from './helpers/findBotCommand';
 import { findOrCreateCommand } from './helpers/findOrCreateCommand';
 import { generateCommandMessage } from './helpers/generateCommandMessage';
+import { hasBotCommandParams } from './helpers/hasBotCommandParams';
 import { sendChatMessage } from './helpers/sendChatMessage';
 
 export const command: BotCommand = {
@@ -8,10 +10,20 @@ export const command: BotCommand = {
   id: 'command',
   description: 'Use this command to find out more about a command',
   callback: async (connection, parsedCommand) => {
-    const commandData = await findOrCreateCommand(parsedCommand.botCommand.id);
-    const message = generateCommandMessage(parsedCommand.commandName, parsedCommand.botCommand, commandData);
-
-    sendChatMessage(connection, message);
+    if (hasBotCommandParams(parsedCommand.parsedMessage)) {
+      const commandToGetDetailsAbout = parsedCommand.parsedMessage.command?.botCommandParams;
+      if (!commandToGetDetailsAbout) {
+        return;
+      }
+      const foundBotCommand = findBotCommand(commandToGetDetailsAbout);
+      if (!foundBotCommand) {
+        sendChatMessage(connection, "I don't know about that command");
+        return;
+      }
+      const commandData = await findOrCreateCommand(foundBotCommand.id);
+      const message = generateCommandMessage(commandToGetDetailsAbout, foundBotCommand, commandData);
+      sendChatMessage(connection, message);
+    }
   },
   cooldown: 5000,
 };
