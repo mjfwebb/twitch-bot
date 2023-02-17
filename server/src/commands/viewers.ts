0563ddeb6eea7a7f0d66c62c39f31ec19497de19
mjfwebb <michael@w3bb.net>
2023-02-17 12:04:25 +0100

    feat: exclude streamer from viewer count

--- server/src/commands/viewers.ts
+++ server/src/commands/viewers.ts
@@ -3,6 +3,7 @@ import { fetchChatters } from '../handlers/twitch/helix/fetchChatters';
 import { sendChatMessage } from './helpers/sendChatMessage';
 import type { BotCommand } from '../types';
 import { getTwitchViewerBotNames } from '../handlers/twitchinsights/twitchViewerBots';
+import Config from '../config';
 
 export const viewers: BotCommand = {
   command: 'viewers',
@@ -10,7 +11,9 @@ export const viewers: BotCommand = {
   cooldown: 5 * SECOND_MS,
   callback: async (connection) => {
     const chatters = await fetchChatters();
-    const viewerCount = chatters.filter((chatter) => !getTwitchViewerBotNames().includes(chatter.user_login)).length;
+    const viewerCount = chatters.filter(
+      (chatter) => !getTwitchViewerBotNames().includes(chatter.user_login) && chatter.user_login !== Config.twitch.account,
+    ).length;
     sendChatMessage(connection, `Currently there are ${viewerCount} viewers 👀`);
   },
 };
