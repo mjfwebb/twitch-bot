d2e0eee21c7f9d6734b3f37006ed4f45be38568f
mjfwebb <michael@w3bb.net>
2023-06-13 17:11:27 +0200

    feat: add nick and avatarUrl to UsersSchema

--- server/src/models/user-model.ts
+++ server/src/models/user-model.ts
@@ -3,12 +3,14 @@ import { model, Schema } from 'mongoose';
 
 const UserSchema = new Schema<User>(
   {
-    userId: { type: String, required: true },
+    userId: { type: String, required: true, unique: true },
+    nick: { type: String, required: false },
     displayName: { type: String, required: false },
     welcomeMessage: { type: String, required: false },
     points: { type: Number, required: true, default: 0 },
     experience: { type: Number, required: true, default: 0 },
     lastSeen: { type: String, required: true, default: '0' },
+    avatarUrl: { type: String, required: false },
   },
   {
     timestamps: true,
@@ -17,11 +19,13 @@ const UserSchema = new Schema<User>(
 
 export interface User extends SchemaTimestampsConfig {
   userId: string;
+  nick: string;
   displayName: string;
   welcomeMessage?: string;
   points: number;
   experience: number;
   lastSeen: string;
+  avatarUrl: string;
 }
 
 const UserModel = model<User>('User', UserSchema);
