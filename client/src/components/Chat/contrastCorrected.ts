b1b70ddd6c6fdc0bd8babec10bd2c5b7b26f49f0
mjfwebb <michael@w3bb.net>
2023-06-16 19:06:29 +0200

    feat: take into account set background color when determining contrast

--- client/src/components/Chat/contrastCorrected.ts
+++ client/src/components/Chat/contrastCorrected.ts
@@ -1,13 +1,25 @@
 type RGB = {
-  r: number;
-  g: number;
-  b: number;
+  red: number;
+  green: number;
+  blue: number;
 };
 
-type Hex = `#${string | number}`;
+type HSL = {
+  hue: number;
+  saturation: number;
+  lightness: number;
+};
+
+function rgbToString({ red, green, blue }: RGB): string {
+  return `rgb(${red},${green},${blue})`;
+}
+
+function hslToString({ hue, saturation, lightness }: HSL): string {
+  return `hsl(${hue}deg ${saturation}% ${lightness}%)`;
+}
 
 // Kudos to Alvaro Montoro https://dev.to/alvaromontoro/building-your-own-color-contrast-checker-4j7o
-export const contrastCorrected = (foregroundHex: string, backgroundHex: Hex): string => {
+export const contrastCorrected = (foregroundHex: string, backgroundHex: string): string => {
   const foregroundRGB = convertHexToRgb(foregroundHex);
   const backgroundRGB = convertHexToRgb(backgroundHex);
 
@@ -17,12 +29,26 @@ export const contrastCorrected = (foregroundHex: string, backgroundHex: Hex): st
 
   const foregroundLuminance = getLuminance(foregroundRGB);
   const backgroundLuminance = getLuminance(backgroundRGB);
-  const constrastRatio = getConstrastRatio(foregroundLuminance, backgroundLuminance);
+  const constrastRatio = getContrastRatio(foregroundLuminance, backgroundLuminance);
 
   const badConstrast = constrastRatio > 1 / 4.5;
 
   if (badConstrast) {
-    return convertRGBtoHSL(foregroundRGB);
+    const hsl = convertRGBtoHSL(foregroundRGB);
+    if (
+      (foregroundLuminance >= backgroundLuminance && backgroundLuminance > 0.5) ||
+      (backgroundLuminance >= foregroundLuminance && backgroundLuminance > 0.5)
+    ) {
+      hsl.lightness = hsl.lightness - 50 * constrastRatio;
+    }
+    if (
+      (foregroundLuminance >= backgroundLuminance && backgroundLuminance < 0.5) ||
+      (backgroundLuminance >= foregroundLuminance && backgroundLuminance < 0.5)
+    ) {
+      hsl.lightness = hsl.lightness + 50 * constrastRatio;
+    }
+    const result = hslToString(hsl);
+    return result;
   }
 
   return foregroundHex;
@@ -37,36 +63,36 @@ function convertHexToRgb(hex: string): RGB | null {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(parsedHex);
   return result
     ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
+        red: parseInt(result[1], 16),
+        green: parseInt(result[2], 16),
+        blue: parseInt(result[3], 16),
       }
     : null;
 }
 
-function getLuminance({ r, g, b }: RGB): number {
-  const a = [r, g, b].map(function (value) {
+function getLuminance({ red, green, blue }: RGB): number {
+  const a = [red, green, blue].map(function (value) {
     value /= 255;
     return value <= 0.03928 ? value / 12.92 : Math.pow((value + 0.055) / 1.055, 2.4);
   });
   return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
-function getConstrastRatio(foregroundLuminance: number, backgroundLuminance: number): number {
+function getContrastRatio(foregroundLuminance: number, backgroundLuminance: number): number {
   return foregroundLuminance > backgroundLuminance
     ? (backgroundLuminance + 0.05) / (foregroundLuminance + 0.05)
     : (foregroundLuminance + 0.05) / (backgroundLuminance + 0.05);
 }
 
-function convertRGBtoHSL({ r, g, b }: RGB): string {
-  // Make r, g, and b fractions of 1
-  r /= 255;
-  g /= 255;
-  b /= 255;
+function convertRGBtoHSL({ red, green, blue }: RGB): HSL {
+  // Make red, green, and blue fractions of 1
+  red /= 255;
+  green /= 255;
+  blue /= 255;
 
   // Find greatest and smallest channel values
-  const chromaMin = Math.min(r, g, b);
-  const chromaMax = Math.max(r, g, b);
+  const chromaMin = Math.min(red, green, blue);
+  const chromaMax = Math.max(red, green, blue);
   const delta = chromaMax - chromaMin;
   let hue = 0;
 
@@ -74,33 +100,37 @@ function convertRGBtoHSL({ r, g, b }: RGB): string {
   // No difference
   if (delta === 0) {
     hue = 0;
-  } else if (chromaMax === r) {
+  } else if (chromaMax === red) {
     // Red is max
-    hue = ((g - b) / delta) % 6;
-  } else if (chromaMax === g) {
+    hue = ((green - blue) / delta) % 6;
+  } else if (chromaMax === green) {
     // Green is max
-    hue = (b - r) / delta + 2;
+    hue = (blue - red) / delta + 2;
   } else {
     // Blue is max
-    hue = (r - g) / delta + 4;
+    hue = (red - green) / delta + 4;
   }
 
-  hue = Math.round(hue * 60);
+  // Keep 2 decimal places for accuracy
+  hue = +(hue * 60).toFixed(2);
 
   // Make negative hues positive behind 360°
-  if (hue < 0) hue += 360;
+  if (hue < 0) {
+    hue += 360;
+  }
 
   // Calculate lightness
   let lightness = 0;
   lightness = (chromaMax + chromaMin) / 2;
-  // Multiply lightness by 100
-  lightness = +(lightness * 100).toFixed(1);
 
   // Calculate saturation
   let saturation = 0;
   saturation = delta === 0 ? 0 : delta / (1 - Math.abs(2 * lightness - 1));
-  // Multiply saturation by 100
-  saturation = +(saturation * 100).toFixed(1);
 
-  return `hsl(${hue},${saturation}%,${73.5}%)`;
+  // Multiply lightness by 100, keep 2 decimal places for accuracy
+  lightness = +(lightness * 100).toFixed(2);
+  // Multiply saturation by 100, keep 2 decimal places for accuracy
+  saturation = +(saturation * 100).toFixed(2);
+
+  return { hue, saturation, lightness };
 }
