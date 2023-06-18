5ef1bf7c71cc30dd7e96b4dd01426bbdd4c8af51
mjfwebb <michael@w3bb.net>
2023-06-18 21:54:33 +0200

    feat: add single start script for both client and server

---
+++ start.mjs
@@ -0,0 +1,24 @@
+import { spawn } from 'child_process';
+
+function npmStart(prefix, directory) {
+  const process = spawn('npm', ['start'], { cwd: directory, shell: true });
+
+  process.stdout.on('data', data => {
+      console.log(`${prefix}: ${data}`);
+  });
+
+  process.stderr.on('data', data => {
+      console.error(`${prefix}: ${data}`);
+  });
+
+  process.on('close', code => {
+      console.log(`child process exited with code ${code}`);
+  });
+
+  process.on('error', err => {
+      console.error(`Failed to start subprocess in ${directory}. Error:`, err);
+  });
+}
+
+npmStart('CLIENT','./client');
+npmStart('SERVER','./server');
