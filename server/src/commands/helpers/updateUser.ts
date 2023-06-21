965b897e998db2b795d2315bd5bb40a3de363f59
mjfwebb <michael@w3bb.net>
2023-06-21 18:41:04 +0200

    refactor: remove unused function updateUserById

--- server/src/commands/helpers/updateUser.ts
+++ server/src/commands/helpers/updateUser.ts
@@ -1,6 +1,6 @@
 import type { HydratedDocument } from 'mongoose';
 import Config from '../../config';
-import { fetchUserInformationById, fetchUserInformationByName } from '../../handlers/twitch/helix/fetchUserInformation';
+import { fetchUserInformationByName } from '../../handlers/twitch/helix/fetchUserInformation';
 import { getTwitchViewerBotNames } from '../../handlers/twitchinsights/twitchViewerBots';
 import type { User } from '../../models/user-model';
 import UserModel from '../../models/user-model';
@@ -20,26 +20,6 @@ export async function updateUserByName(displayName: string): Promise<HydratedDoc
   } else {
     const userInformation = await fetchUserInformationByName(displayName);
     if (userInformation && userInformation.id && userInformation.display_name && userInformation.profile_image_url) {
-      user.avatarUrl = userInformation.profile_image_url;
-      await user.save();
-      return user;
-    }
-  }
-  return null;
-}
-
-export async function updateUserById(userId: string): Promise<HydratedDocument<User> | null> {
-  if (getTwitchViewerBotNames().includes(userId)) {
-    return null;
-  }
-
-  const user = await UserModel.findOne({ userId });
-  if (!user) {
-    return null;
-  } else {
-    const userInformation = await fetchUserInformationById(userId);
-    if (userInformation && userInformation.id && userInformation.display_name && userInformation.profile_image_url) {
-      user.displayName = userInformation.display_name;
       user.avatarUrl = userInformation.profile_image_url;
       await user.save();
       return user;
