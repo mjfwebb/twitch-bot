f3ffacb56b151af0f1635c480ecb6dcf753f0ffb
mjfwebb <michael@w3bb.net>
2023-07-26 18:24:58 +0200

    feat: significantly reduce time it takes for the server to start

--- start.mjs
+++ start.mjs
@@ -20,5 +20,25 @@ function npmStart(prefix, directory) {
   });
 }
 
+function tsNodeStart(prefix, directory) {
+    const process = spawn('npx ts-node', ['--transpiler', 'sucrase/ts-node-plugin', 'src/index.ts'], { cwd: directory, shell: true });
+
+    process.stdout.on('data', data => {
+        console.log(`${prefix}: ${data}`);
+    });
+  
+    process.stderr.on('data', data => {
+        console.error(`${prefix}: ${data}`);
+    });
+  
+    process.on('close', code => {
+        console.log(`child process exited with code ${code}`);
+    });
+  
+    process.on('error', err => {
+        console.error(`Failed to start subprocess in ${directory}. Error:`, err);
+    });
+}
+
 npmStart('CLIENT','./client');
-npmStart('SERVER','./server');
+tsNodeStart('SERVER','./server');
