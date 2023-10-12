ae395413d36877621386acad0a9ec6f0a376474f
mjfwebb <michael@w3bb.net>
2023-10-12 19:10:29 +0200

    feat: add support for spotify.link URLs in queuesong command

--- server/src/commands/queuesong.ts
+++ server/src/commands/queuesong.ts
@@ -1,3 +1,4 @@
+import { parseHTML } from 'linkedom';
 import { SECOND_MS } from '../constants';
 import { addSongToPlaybackQueue } from '../handlers/spotify/addSongToPlaybackQueue';
 import { getTrack } from '../handlers/spotify/getTrack';
@@ -17,25 +18,47 @@ export const queuesong: BotCommand = {
       const trackInput = parsedCommand.parsedMessage.command.botCommandParams;
 
       // Define the start of a spotify url and uri
-      const urlStart = 'https://open.spotify.com/track/';
+      const openSpotifyTrackUrl = 'https://open.spotify.com/track/';
+      const spotifyLinkUrl = 'https://spotify.link/';
       const trackStart = 'spotify:track:';
 
       // By default, assume the user has pasted a spotify uri, extract the track id
       let trackId = trackInput.slice(trackStart.length);
 
-      // If the user has pasted a spotify url, extract the track id
-      if (trackInput.startsWith(urlStart)) {
-        trackId = trackInput.slice(urlStart.length);
+      // If the user has pasted an open.spotify.com url, extract the track id
+      if (trackInput.startsWith(openSpotifyTrackUrl)) {
+        trackId = trackInput.slice(openSpotifyTrackUrl.length);
 
         // If the url has a query string, remove it
         if (trackId.includes('?')) {
           trackId = trackId.split('?')[0];
         }
       }
+      
+      // If the user has pasted a spotiy.link url, make a request to the spotify.link api to get the track id
+      if (trackInput.startsWith(spotifyLinkUrl)) {
+        const spotifyLinkUrlResponse = await fetch(trackInput);
+
+        if (spotifyLinkUrlResponse.status !== 200) {
+          sendChatMessage(connection, `Something went wrong adding the song to the queue athanoSad. Try again?`);
+          return;
+        }
+
+        const htmlResponse = await spotifyLinkUrlResponse.text();
+        const actionHref = parseHTML(htmlResponse).window.document.querySelector('a.action')?.getAttribute('href');
+
+        if (!actionHref) {
+          sendChatMessage(connection, `Something went wrong adding the song to the queue athanoSad. Try again?`);
+          return;
+        }
+
+        // trackId format will be like track/trackId?_branch_referrer=stuff
+        trackId = actionHref.split('/')[1].split('?')[0];
+      }
 
       // If the input is neither a url or a uri, send a message to the chat and exit
-      if (!trackInput.startsWith(trackStart) && !trackInput.startsWith(urlStart)) {
-        sendChatMessage(connection, `That doesn't look right... athanoThink it needs to be like ${trackStart}stuff or ${urlStart}stuff`);
+      if (!trackInput.startsWith(trackStart) && !trackInput.startsWith(openSpotifyTrackUrl)  && !trackInput.startsWith(spotifyLinkUrl)) {
+        sendChatMessage(connection, `That doesn't look right... athanoThink it needs to be like ${trackStart}stuff, or ${openSpotifyTrackUrl}stuff, or ${spotifyLinkUrl}stuff`);
         return;
       }
 
