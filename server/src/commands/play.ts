252becf45dbb82af6e318739a6dc4fff1da9791d
Michael Webb <28074382+mjfwebb@users.noreply.github.com>
2023-11-15 12:37:09 +0100

    Update play.ts
    use the length of the links array for the upper-bound when getting the random number

--- server/src/commands/play.ts
+++ server/src/commands/play.ts
@@ -8,7 +8,7 @@ export const play: BotCommand = {
   description: 'Get the play link for Between Worlds',
   callback: (connection) => {
     const links = ['https://www.betweenworlds.net', 'https://athanoquest.com', 'https://athano.quest'];
-    const number = getRandomNumberInRange(0, 3);
+    const number = getRandomNumberInRange(0, links.length -1);
     sendChatMessage(connection, links[number]);
   },
 };
