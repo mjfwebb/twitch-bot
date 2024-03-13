5a02f2e3d422f1725c424c4f1ea26c75dcce7ac5
mjfwebb <michael@w3bb.net>
2024-03-13 22:00:47 +0100

    feat: add p! BTTV party emote modifier

--- client/src/views/Chat/bttvModifierFlags.ts
+++ client/src/views/Chat/bttvModifierFlags.ts
@@ -1,4 +1,4 @@
-export const bttvModifiers = ['w!', 'h!', 'v!', 'z!', 'c!', 'l!', 'r!'];
+export const bttvModifiers = ['w!', 'h!', 'v!', 'z!', 'c!', 'l!', 'r!', 'p!'];
 type BTTVModifier = (typeof bttvModifiers)[number];
 
 export const bttvModifierMap: Record<BTTVModifier, string> = {
@@ -9,4 +9,5 @@ export const bttvModifierMap: Record<BTTVModifier, string> = {
   'l!': 'rotateLeft',
   'z!': 'zerowidth',
   'c!': 'cursed',
+  'p!': 'party',
 };
