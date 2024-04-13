09522ce0d313bf36854ecc1158cb4f65f4814003
mjfwebb <michael@w3bb.net>
2024-04-13 13:56:46 +0200

    feat: add ChannelChatNotificationEvent event

--- server/src/typings/twitchEvents.ts
+++ server/src/typings/twitchEvents.ts
@@ -19,7 +19,8 @@ export type EventsubEvent =
   | ChannelPointsCustomRewardRedemptionEvent
   | ChannelSubscribeMessageEvent
   | ChannelChatMessageEvent
-  | ChannelChatMessageDeleteEvent;
+  | ChannelChatMessageDeleteEvent
+  | ChannelChatNotificationEvent;
 
 export type EventsubEventBase<EventType extends EventsubSubscriptionType> = {
   eventType: EventType;
@@ -70,7 +71,8 @@ export type EventsubSubscriptionType =
   // | 'user.authorization.revoke'
   // | 'user.update'
   | 'channel.chat.message'
-  | 'channel.chat.message_delete';
+  | 'channel.chat.message_delete'
+  | 'channel.chat.notification';
 
 type EmotePlacement = {
   begin: number; //	The index of where the Emote starts in the text.
@@ -253,4 +255,123 @@ interface ChannelChatMessageDeleteEvent extends EventsubEventBase<'channel.chat.
   target_user_name: string; // The user name of the user whose message was deleted.
   target_user_login: string; // The user login of the user whose message was deleted.
   message_id: string; // A UUID that identifies the message that was removed.
+}
+
+interface ChannelChatNoticeSubscription {
+  sub_tier: '1000' | '2000' | '3000'; // The type of subscription plan being used. Possible values are: 1000 - First level of paid or Prime subscription. 2000 - Second level of paid subscription. 3000 - Third level of paid subscription.
+  is_prime: boolean; // Indicates if the subscription was obtained through Amazon Prime.
+  duration_months: number; // The number of months the subscription is for.
+}
+
+interface ChannelChatNoticeResubscription {
+  cumulative_months: number; // The total number of months the user has subscribed.
+  duration_months: number; // The number of months the subscription is for.
+  streak_months: number; // The total number of months the user has subscribed.
+  sub_tier: '1000' | '2000' | '3000'; // The type of subscription plan being used. Possible values are: 1000 - First level of paid or Prime subscription. 2000 - Second level of paid subscription. 3000 - Third level of paid subscription.
+  is_prime: boolean; // Indicates if the subscription was obtained through Amazon Prime.
+  is_gift: boolean; // Whether or not the resub was a result of a gift.
+  gifter_is_anonymous: boolean; // Whether or not the gift was anonymous.
+  gifter_user_id: string; // The user ID of the subscription gifter. Null if anonymous.
+  gifter_user_name: string; // The user name of the subscription gifter. Null if anonymous.
+  gifter_user_login: string; // The user login of the subscription gifter. Null if anonymous.
+}
+
+interface ChannelChatNoticeSubscriptionGift {
+  duration_months: number; // The number of months the subscription is for.
+  cumulative_total: number; // The amount of gifts the gifter has given in this channel. Null if anonymous.
+  recipient_user_id: string; // The user ID of the subscription gift recipient.
+  recipient_user_name: string; // The user name of the subscription gift recipient.
+  recipient_user_login: string; // The user login of the subscription gift recipient.
+  sub_tier: string; // The type of subscription plan being used. Possible values are: 1000 - First level of paid or Prime subscription. 2000 - Second level of paid subscription. 3000 - Third level of paid subscription.
+  community_gift_id: string; // The ID of the associated community gift. Null if not associated with a community gift.
+}
+
+interface ChannelChatNoticeSubscriptionCommunityGift {
+  id: string; // The ID of the associated community gift.
+  total: number; // Number of subscriptions being gifted.
+  sub_tier: string; // The type of subscription plan being used. Possible values are: 1000 - First level of paid or Prime subscription. 2000 - Second level of paid subscription. 3000 - Third level of paid subscription.
+  cumulative_total: number; // The amount of gifts the gifter has given in this channel. Null if anonymous.
+}
+
+interface ChannelChatNoticeGiftPaidUpgrade {
+  gifter_is_anonymous: boolean; // Whether the gift was given anonymously.
+  gifter_user_id: string; // The user ID of the user who gifted the subscription. Null if anonymous.
+  gifter_user_name: string; // The user name of the user who gifted the subscription. Null if anonymous.
+  gifter_user_login: string; // The user login of the user who gifted the subscription. Null if anonymous.
+}
+
+interface ChannelChatNoticePrimePaidUpgrade {
+  sub_tier: string; // The type of subscription plan being used. Possible values are: 1000 - First level of paid or Prime subscription. 2000 - Second level of paid subscription. 3000 - Third level of paid subscription.
+}
+
+interface ChannelChatNoticeRaid {
+  user_id: string; // The user ID of the broadcaster raiding this channel.
+  user_name: string; // The user name of the broadcaster raiding this channel.
+  user_login: string; // The login name of the broadcaster raiding this channel.
+  viewer_count: number; // The number of viewers raiding this channel from the broadcaster’s channel.
+  profile_image_url: string; // Profile image URL of the broadcaster raiding this channel.
+}
+
+interface ChannelChatNoticePayItForward {
+  gifter_is_anonymous: boolean; // Whether the gift was given anonymously.
+  gifter_user_id: string; // The user ID of the user who gifted the subscription. Null if anonymous.
+  gifter_user_name: string; // The user name of the user who gifted the subscription. Null if anonymous.
+  gifter_user_login: string; // The user login of the user who gifted the subscription. Null if anonymous.
+}
+
+interface ChannelChatNoticeAnnouncement {
+  color: string; // Color of the announcement.
+}
+
+interface ChannelChatNoticeCharityDonation {
+  charity_name: string; // Name of the charity.
+  amount: {
+    value: number; // The monetary amount. The amount is specified in the currency’s minor unit. For example, the minor units for USD is cents, so if the amount is $5.50 USD, value is set to 550.
+    decimal_place: number; // The number of decimal places used by the currency. For example, USD uses two decimal places.
+    currency: string; // The ISO-4217 three-letter currency code that identifies the type of currency in value.
+  };
+}
+
+interface ChannelChatNoticeBitsBadgeTier {
+  tier: number; // The tier of the Bits badge the user just earned. For example, 100, 1000, or 10000.
+}
+
+interface ChannelChatNotificationEvent extends EventsubEventBase<'channel.chat.notification'> {
+  broadcaster_user_id: string; // The broadcaster user ID.
+  broadcaster_user_name: string; // The broadcaster display name.
+  broadcaster_user_login: string; // The broadcaster login.
+  chatter_user_id: string; // The user ID of the user that sent the message.
+  chatter_user_name: string; // The user name of the user that sent the message.
+  chatter_user_login: string; // The user login of the user that sent the message.
+  chatter_is_anonymous: boolean; // Whether or not the chatter is anonymous.
+  color: string; // The color of the user’s name in the chat room.
+  badges: ChatBadge[]; // List of chat badges.
+  system_message: string; // The message Twitch shows in the chat room for this notice.
+  message_id: string; // A UUID that identifies the message.
+  message: ChatMessage; // The structured chat message.
+  notice_type:
+    | 'sub'
+    | 'resub'
+    | 'sub_gift'
+    | 'community_sub_gift'
+    | 'gift_paid_upgrade'
+    | 'prime_paid_upgrade'
+    | 'raid'
+    | 'unraid'
+    | 'pay_it_forward'
+    | 'announcement'
+    | 'bits_badge_tier'
+    | 'charity_donation'; // The type of notice.
+  sub?: ChannelChatNoticeSubscription; // Information about the sub event. Null if notice_type is not sub.
+  resub?: ChannelChatNoticeResubscription; // Information about the resub event. Null if notice_type is not resub.
+  sub_gift?: ChannelChatNoticeSubscriptionGift; // Information about the gift sub event. Null if notice_type is not sub_gift.
+  community_sub_gift?: ChannelChatNoticeSubscriptionCommunityGift; // Information about the community gift sub event. Null if notice_type is not community_sub_gift.
+  gift_paid_upgrade?: ChannelChatNoticeGiftPaidUpgrade; // Information about the community gift paid upgrade event. Null if notice_type is not gift_paid_upgrade.
+  prime_paid_upgrade?: ChannelChatNoticePrimePaidUpgrade; // Information about the Prime gift paid upgrade event. Null if notice_type is not prime_paid_upgrade.
+  raid?: ChannelChatNoticeRaid; // Information about the raid event. Null if notice_type is not raid.
+  unraid?: Record<string, never>; // Returns an empty payload if notice_type is unraid, otherwise returns null.
+  pay_it_forward?: ChannelChatNoticePayItForward; // Information about the pay it forward event. Null if notice_type is not pay_it_forward.
+  announcement?: ChannelChatNoticeAnnouncement; // Information about the announcement event. Null if notice_type is not announcement.
+  charity_donation?: ChannelChatNoticeCharityDonation; // Information about the charity donation event. Null if notice_type is not charity_donation.
+  bits_badge_tier?: ChannelChatNoticeBitsBadgeTier; // Information about the bits badge tier event. Null if notice_type is not bits_badge_tier.
 }
