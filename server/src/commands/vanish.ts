b56294ff01cbe91c4e0e2239c1741c8d9454d939
Adrian <107351903+6lr61@users.noreply.github.com>
2024-07-28 15:15:56 +0200

    fix: moderators lose their sword if timed out

--- server/src/commands/vanish.ts
+++ server/src/commands/vanish.ts
@@ -1,5 +1,6 @@
 import { MINUTE_MS } from '../constants';
 import { banUser } from '../handlers/twitch/helix/moderation';
+import { sendChatMessage } from './helpers/sendChatMessage';
 import type { BotCommand } from '../types';
 
 const DURATION_S = 5;
@@ -10,6 +11,13 @@ export const vanish: BotCommand = {
   cooldown: 5 * MINUTE_MS,
   callback: (connection, parsedCommand) => {
     const userId = parsedCommand.parsedMessage.tags?.['user-id'];
+    const isModerator = parsedCommand.parsedMessage.tags?.mod === '1';
+
+    if (isModerator) {
+      sendChatMessage(connection, "I won't timeout a moderator!");
+      return;
+    }
+
     if (userId) {
       void banUser(userId, DURATION_S);
     }
