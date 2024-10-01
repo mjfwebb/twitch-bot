359a3b4ecca70670ac289ce16879e9bef5e41224
mjfwebb <michael@w3bb.net>
2024-10-01 23:23:16 +0200

    refactor: clean up spotify auth flow to not imply tokens are returned

--- server/src/auth/spotify.ts
+++ server/src/auth/spotify.ts
@@ -38,11 +38,11 @@ interface FetchWithRetryOptions {
 
 export const fetchWithRetry = async ({ url, init, attemptNumber = 0, shouldJSONParse = true }: FetchWithRetryOptions): Promise<unknown> => {
   if (rateLimitReached) {
-    throw new Error('Spotify API rate limit reached. Waiting for rate limit to be lifted.');
+    throw new Error('Spotify: API rate limit reached. Waiting for rate limit to be lifted.');
   }
 
   if (attemptNumber > 1) {
-    throw new Error(`Failed to perform fetch ${attemptNumber} ${simplePluralise('time', attemptNumber)} to Spotify API`);
+    throw new Error(`Spotify: Failed to perform fetch ${attemptNumber} ${simplePluralise('time', attemptNumber)} to Spotify API`);
   }
   const result = await fetch(url, init);
 
@@ -56,7 +56,7 @@ export const fetchWithRetry = async ({ url, init, attemptNumber = 0, shouldJSONP
     if (retryAfter) {
       const retryAfterSeconds = parseInt(retryAfter, 10);
       if (retryAfterSeconds) {
-        logger.info(`Spotify API rate limit reached. Waiting ${retryAfterSeconds} seconds before allowing another request.`);
+        logger.info(`Spotify: API rate limit reached. Waiting ${retryAfterSeconds} seconds before allowing another request.`);
         setTimeout(() => {
           rateLimitReached = false;
         }, retryAfterSeconds * 1000);
@@ -70,46 +70,43 @@ export const fetchWithRetry = async ({ url, init, attemptNumber = 0, shouldJSONP
       data = await result.json();
     }
   } catch (error) {
-    logger.error(`Unable to parse JSON response from Spotify API. Error: ${errorMessage(error)}. Response: ${JSON.stringify(result)}`);
+    logger.error(`Spotify: Unable to parse JSON response from API. Error: ${errorMessage(error)}. Response: ${JSON.stringify(result)}`);
 
     throw error;
   }
-  const shouldRetry = checkResponseForErrors(data);
-  if (shouldRetry) {
-    return fetchWithRetry({
-      url,
-      init,
-      attemptNumber: attemptNumber + 1,
-      shouldJSONParse,
-    });
+  assertNotBadRequest(data);
+
+  if (data !== null && hasOwnProperty(data, 'error') && hasOwnProperty(data.error, 'status')) {
+    if (data.error.status === StatusCodes.UNAUTHORIZED) {
+      // NOTE: Access token may have expired. Try to refresh.
+      refreshAccessToken(Config.spotify).catch((e) => logger.error(e));
+
+      return fetchWithRetry({
+        url,
+        init,
+        attemptNumber: attemptNumber + 1,
+        shouldJSONParse,
+      });
+    }
   } else {
     return data;
   }
 };
 
 /**
- * Checks the data response for any errors and performs necessary actions based on the error status.
- * @param data - The data response object.
- * @returns A boolean indicating whether an error was found.
- * @throws Error with a specific message if a bad request error is encountered.
- * @remarks If the error status is UNAUTHORIZED, it attempts to refresh the Access Token.
+ * Checks if the data response contains a status code of 400 Bad Request and throws an error if it does.
+ * @param data
+ * @throws Error if the data response contains a status code of 400 Bad Request
  */
-export const checkResponseForErrors = (data: unknown): boolean => {
+const assertNotBadRequest = (data: unknown): void => {
   if (data !== null && hasOwnProperty(data, 'error') && hasOwnProperty(data.error, 'status')) {
-    assert(typeof data.error.status === 'number', 'status in data response is not a number');
+    assert(typeof data.error.status === 'number', 'Spotify: status in data response is not a number');
     if (data.error.status === StatusCodes.BAD_REQUEST) {
-      assert(hasOwnProperty(data, 'message'), 'message not found in data response');
-      assert(typeof data.message === 'string', 'message in data response is not a number');
-      throw new Error(`Bad request: ${data.message}`);
-    }
-
-    if (data.error.status === StatusCodes.UNAUTHORIZED) {
-      // NOTE: Access token may have expired. Try to refresh.
-      refreshAccessToken(Config.spotify).catch((e) => logger.error(e));
-      return true;
+      assert(hasOwnProperty(data, 'message'), 'Spotify: message not found in data response');
+      assert(typeof data.message === 'string', 'Spotify: message in data response is not a number');
+      throw new Error(`Spotify: Bad request sent to API: ${data.message}`);
     }
   }
-  return false;
 };
 
 /**
@@ -118,10 +115,9 @@ export const checkResponseForErrors = (data: unknown): boolean => {
  * @returns The access token as a string.
  * @throws AssertionError if the access_token property is missing or not of the expected type.
  */
-
 const parseNewAccessToken = (data: unknown): string => {
-  assert(hasOwnProperty(data, 'access_token'), 'access_token not found in data response');
-  assert(typeof data.access_token === 'string', 'access_token in data response is not a string');
+  assert(hasOwnProperty(data, 'access_token'), 'Spotify: access_token not found in data response');
+  assert(typeof data.access_token === 'string', 'Spotify: access_token in data response is not a string');
   setTokenInFile('spotify_access_token', data.access_token);
   return data.access_token;
 };
@@ -129,17 +125,15 @@ const parseNewAccessToken = (data: unknown): string => {
 /**
  * Parses the new tokens from the data response and updates the corresponding tokens in the file.
  * @param data - The data response object.
- * @returns The access token as a string.
  * @throws AssertionError if any of the required token properties are missing or not of the expected type.
  */
-const parseNewTokens = (data: unknown): string => {
-  assert(hasOwnProperty(data, 'access_token'), 'access_token not found in data response');
-  assert(hasOwnProperty(data, 'refresh_token'), 'refresh_token not found in data response');
-  assert(typeof data.access_token === 'string', 'access_token in data response is not a string');
-  assert(typeof data.refresh_token === 'string', 'refresh_token in data response is not a string');
+const saveNewTokens = (data: unknown): void => {
+  assert(hasOwnProperty(data, 'access_token'), 'Spotify: access_token not found in data response');
+  assert(hasOwnProperty(data, 'refresh_token'), 'Spotify: refresh_token not found in data response');
+  assert(typeof data.access_token === 'string', 'Spotify: access_token in data response is not a string');
+  assert(typeof data.refresh_token === 'string', 'Spotify: refresh_token in data response is not a string');
   setTokenInFile('spotify_access_token', data.access_token);
   setTokenInFile('spotify_refresh_token', data.refresh_token);
-  return data.access_token;
 };
 
 /**
@@ -165,20 +159,19 @@ const refreshAccessToken = async (spotifyConfig: SpotifyConfig): Promise<string>
       },
     });
     const data: unknown = await result.json();
-    checkResponseForErrors(data);
+    assertNotBadRequest(data);
     return parseNewAccessToken(data);
   } catch (error) {
-    throw new Error(`Unable to use Refresh Token to obtain new Access Token from Spotify. Error: ${errorMessage(error)}`);
+    throw new Error(`Spotify: Unable to use Refresh Token to obtain new Access Token. Error: ${errorMessage(error)}`);
   }
 };
 
 /**
  * Obtains a new Access Token from Spotify using the provided configuration.
  * @param spotifyConfig - The Spotify configuration object.
- * @returns A Promise that resolves to the new Access Token as a string.
  * @throws Error if unable to obtain a new Access Token from Spotify.
  */
-const getNewAccessToken = async (spotifyConfig: SpotifyConfig): Promise<string> => {
+const getNewAccessToken = async (spotifyConfig: SpotifyConfig): Promise<void> => {
   try {
     if (spotifyConfig === null) {
       throw new Error('Spotify configuration is missing.');
@@ -193,32 +186,37 @@ const getNewAccessToken = async (spotifyConfig: SpotifyConfig): Promise<string> 
     });
 
     const data: unknown = await result.json();
-    checkResponseForErrors(data);
-    return parseNewTokens(data);
+
+    console.log(data);
+
+    assertNotBadRequest(data);
+    saveNewTokens(data);
   } catch (error) {
-    throw new Error(`Unable to obtain new Access Token from Spotify. Error: ${errorMessage(error)}`);
+    throw new Error(`Spotify: Unable to obtain new Access Token. Error: ${errorMessage(error)}`);
   }
 };
 
 /**
  * Retrieves the Spotify access token.
- * @returns A Promise that resolves to the Spotify access token as a string.
  * @throws Error if unable to get the Spotify Access Token.
  */
-export const getSpotifyAccessToken = async (): Promise<string> => {
+export const getSpotifyAccessToken = async (): Promise<void> => {
   const accessToken = getTokenFromFile('spotify_access_token');
+  // If the access token is already set, we don't need to do anything
   if (accessToken) {
-    return accessToken;
+    return;
   }
   try {
     const refreshToken = getTokenFromFile('spotify_refresh_token');
     if (refreshToken) {
-      return await refreshAccessToken(Config.spotify);
+      // If we have a refresh token, we can use it to get a new access token
+      await refreshAccessToken(Config.spotify);
     } else {
-      return await getNewAccessToken(Config.spotify);
+      // If we don't have a refresh token, we need to get a new access token
+      await getNewAccessToken(Config.spotify);
     }
   } catch (error) {
-    throw new Error(`Unable to get Spotify Access Token. Error: ${errorMessage(error)}`);
+    throw new Error(`Spotify: Unable to get Access Token. Error: ${errorMessage(error)}`);
   }
 };
 
@@ -236,16 +234,16 @@ export const spotifyAuthCodeRouter = async () => {
           });
 
           res.send(
-            'Hello from twitch-bot! Spotify auth code received and your configuration has been updated.' +
+            'Hello from twitch-bot! Spotify Auth Code received and your configuration has been updated.' +
               'You may close this window. Please restart the bot.',
           );
         } else {
-          res.send('Hello from twitch-bot! No Spotify auth code received. You may close this window.');
+          res.send('Hello from twitch-bot! No Spotify Auth Code received. You may close this window.');
         }
       })
       .listen(3000);
 
-    logger.info(`Getting Spotify auth code with scopes ${pc.green(`${Config.twitch.scopes.join(', ')}`)}`);
+    logger.info(`Spotify: Getting Auth Code with scopes ${pc.green(`${Config.twitch.scopes.join(', ')}`)}`);
     await getSpotifyAuthCode();
   }
 };
@@ -256,6 +254,6 @@ const getSpotifyAuthCode = async (): Promise<void> => {
     const url = `${SPOTIFY_AUTH_URL}authorize?client_id=${Config.spotify.client_id}&response_type=code&redirect_uri=${Config.spotify.redirect_uri}&scope=${scopes}`;
     await open(url);
   } catch (error) {
-    throw new Error(`Unable to get Spotify auth code. Error: ${errorMessage(error)}`);
+    throw new Error(`Spotify: Unable to get Auth Code. Error: ${errorMessage(error)}`);
   }
 };
