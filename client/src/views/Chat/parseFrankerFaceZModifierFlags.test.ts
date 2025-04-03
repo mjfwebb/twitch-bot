93ae445292f8e8292ced0bbb09348ad227afebff
Adrian <107351903+6lr61@users.noreply.github.com>
2025-04-03 14:34:07 +0200

    test: corrected expected value

--- client/src/views/Chat/parseFrankerFaceZModifierFlags.test.ts
+++ client/src/views/Chat/parseFrankerFaceZModifierFlags.test.ts
@@ -10,7 +10,6 @@ describe('parseFrankerFaceZModifierFlags', async () => {
     expect(parseFrankerFaceZModifierFlags(1)).toEqual(['hidden']);
     expect(parseFrankerFaceZModifierFlags(2048)).toEqual(['rainbow']);
     expect(parseFrankerFaceZModifierFlags(12_289)).toEqual(['hypershake', 'hyperred', 'hidden']);
-    // FIXME: This is incorrect?
-    // expect(parseFrankerFaceZModifierFlags(65_606)).toEqual(['bounce', 'growx', 'flipx', 'flipy', 'hidden']);
+    expect(parseFrankerFaceZModifierFlags(65_606)).toEqual(['bounce', 'leave', 'flipy', 'flipx']);
   });
 });
